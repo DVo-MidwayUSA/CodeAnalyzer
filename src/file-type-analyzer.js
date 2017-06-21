144c5ae1d51ce1cba6c1b0c01d9ee2dd2ef72dc1
Vo <DVo@midwayusa.com>
2017-06-21 15:27:40 -0500

    refactored and added support for multiple directories

--- src/file-type-analyzer.js
+++ src/file-type-analyzer.js
@@ -3,42 +3,38 @@
 const _ = require('lodash')
 const glob = require('glob')
 const minimatch = require('minimatch')
+const path = require('path')
+const config = require('./config')
 
-const exclusions = [
-    {name: 'jQuery', pattern: '**/jQuery/**'},
-    {name: 'Node Modules', pattern: 'node_modules/**'},
-    {name: 'Obj', pattern: 'obj/**'},
-    {name: 'Bin', pattern: 'bin/**'},
-    {name: 'App_Data', pattern: 'bin/**'},
-    {name: 'App_GlobalResources', pattern: 'bin/**'},
-]
-
-const fileTypes = [
-    {name: 'C#', pattern: '**/*.cs'},
-    {name: 'JavaScript', pattern: '**/*.js'},
-    {name: 'CSHTML', pattern: '**/*.cshtml'},
-    {name: 'HTML', pattern: '**/*+(.html|*.htm)'},
-]
+const exclusions = config.exclusions
+const fileTypes = config.fileTypes
 
 class FileTypeAnalyzer {
     constructor(directory) {
-        process.chdir(directory)
+        this.directory = directory
     }
 
     analyze() {
-        fileTypes.forEach(this.getFilesByType)
+        fileTypes.forEach(
+            (fileType) => this.getFilesByType(fileType))
     }
 
     getFilesByType(fileType) {
-        glob(fileType.pattern, (error, files) => {
-            exclusions.forEach((exclusion) => {
-                _.remove(files, (file) => {
-                    return minimatch(file, exclusion.pattern)
-                })
-            })
+        glob(fileType.pattern, {cwd: this.directory}, (errors, files) =>
+            this.processExclusions(errors, files, fileType, this.directory))
+    }
 
-            console.log(`${process.cwd()} - ${fileType.name} File Count:`, files.length)
+    processExclusions(error, files, fileType) {
+        exclusions.forEach((exclusion) => {
+            _.remove(files, (file) => {
+                return minimatch(file, exclusion.pattern)
+            })
         })
+
+        let format =
+            `${path.basename(this.directory)} - ${fileType.name} File Count:`
+
+        console.log(format, files.length)
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 'use strict'
 
 const FileTypeAnalyzer = require('./file-type-analyzer')
+const config = require('./config')
 
-const directories = [
-    'C:\\Workspaces\\TFS15\\Website\\Master\\WWW\\WWW.Web',
-]
+const directories = config.directories
 
 directories.forEach((directory) => {
     let analyzer = new FileTypeAnalyzer(directory)
